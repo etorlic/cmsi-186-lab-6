0a3eb68b94506a43b1274ea683aa317bcb2dab1c
Emin Torlic <emin.torlic@icloud.com>
2020-04-24 10:59:13 -0700

    Coin Changer

--- CoinChanger.java
+++ CoinChanger.java
@@ -45,11 +45,8 @@ public abstract class CoinChanger {
             }
 
             results.put(amount, minCoins);
-            // TODO: Do the top-down-with-memoization algorithm here. You should
-            // do this recursively, so write a separate, private, recursive,
-            // "helper" method. This method here will call that recursive
-            // method with the memo object and the initial amount.
-            return minCoins; // TODO change this line, of course
+
+            return minCoins;
         }
     }
 
@@ -60,19 +57,16 @@ public abstract class CoinChanger {
             var table = new int[amount + 1];
 
             // index 0 is known because it takes 0 coins for 0 amount
-            // index 1 is known due to checking if one of denominations contains 1,
-            // so it takes 1 coin of 1 unit to fill an amount of 1
             table[0] = 0;
-            table[1] = 1;
 
             // an arbitrary value to fill the rest of the table
-            for (var t = 2; t <= amount; t++) {
+            for (var t = 1; t <= amount; t++) {
                 table[t] = amount + 1;
             }
 
-            // fills the table up from index 2 and onward with 
+            // fills the table up from index 1 and onward with 
             // mininimum coins for each index
-            for (var i = 2; i <= amount; i++) {
+            for (var i = 1; i <= amount; i++) {
                 for (var d : denominations) {
                     if (i - d < 0) {
                         break;
